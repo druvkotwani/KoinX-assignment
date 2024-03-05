b45c03ca51a546bd18a30ad8b0e2086304e563a7
druvkotwani <druvkotwani12@gmail.com>
2024-03-05 21:17:00 +0530

    logo + text done

--- src/components/HeroSection/Coins/CoinsDetail.jsx
+++ src/components/HeroSection/Coins/CoinsDetail.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BitcoinLogo } from '../../../utils/Icons'
 
 function CoinsDetail() {
     return (
         <div className='bg-white '>
-            <h1>CoinsDetail</h1>
+
+            <div className='flex gap-2 justify-center items-center'>
+                <BitcoinLogo width={24} height={24} />
+
+                {/* Name and Short */}
+                <div className='flex justify-center items-center gap-2'>
+                    <p className='font-semibold text-base '>Bitcoin</p>
+                    <span className='text-xs font-semibold  text-[#5d667b] '>BTC</span>
+                </div>
+                {/* Rank */}
+                <span className='text-xs p-2 rounded-md text-white bg-[#768396]'>Rank #1</span>
+            </div>
+
+            <div>
+
+            </div>
         </div>
     )
 }
