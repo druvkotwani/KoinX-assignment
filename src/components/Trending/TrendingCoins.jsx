e322730029bf06a5dd601e41163fc4287e49f731
druvkotwani <druvkotwani12@gmail.com>
2024-03-06 11:42:32 +0530

    trending coins section completed

--- src/components/Trending/TrendingCoins.jsx
+++ src/components/Trending/TrendingCoins.jsx
@@ -11,26 +11,30 @@ const TrendingCoins = () => {
             })
     }, [])
     return (
-        <div className='bg-white px-4 rounded-md '>
-            <h1 className='font-semibold text-xl '>Trending Coins {`(24H)`}</h1>
+        <div className='bg-white px-4 rounded-md'>
+            <h1 className='font-semibold text-xl pt-4   '>Trending Coins {`(24h)`}</h1>
 
             <ul>
                 {
-                    trendingCoins.map((coin, index) => {
+                    trendingCoins.slice(0, 3).map((coin, index) => {
                         return (
                             <li key={index} className='flex justify-between items-center gap-4 mt-4  '>
                                 <div className='flex justify-center items-center gap-1'>
                                     <img src={coin.item.small} alt={coin.item.name} className='w-6 h-6' />
                                     <span className='font-medium '>{coin.item.name}</span>
+                                    <span>{`(${coin.item.symbol})`}</span>
                                 </div>
-                                <span className='text-sm  px-2 py-1 font-meid bg-[#eaf8f4]  text-[#15b179] rounded-md '>{(coin.item.data.price_change_percentage_24h.usd).toFixed(2)}</span>
+                                <span className={`text-sm  px-2 py-1 font-meid  ${coin.item.data.price_change_percentage_24h.usd > 0 ? `text-[#15b179] bg-[#eaf8f4]` : `text-[red] bg-[#fdeae9]`}
+  rounded-md`}>
+                                    {coin.item.data.price_change_percentage_24h.usd > 0 ? `▲${coin.item.data.price_change_percentage_24h.usd.toFixed(2)}%` : `▼${coin.item.data.price_change_percentage_24h.usd.toFixed(2) * -1}%`}
+                                </span>
                             </li>
                         )
                     })
                 }
 
             </ul>
-        </div>
+        </div >
     )
 }
 
