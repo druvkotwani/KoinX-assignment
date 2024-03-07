e88813cbb0001f779f812d4bd8655b6524c3a05e
druvkotwani <druvkotwani12@gmail.com>
2024-03-07 17:06:59 +0530

    Navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,45 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Close, Hamburger } from '../../utils/Icons'
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
     return (
-        <div className='w-full flex justify-between items-center px-8 bg-white'>
-            {/* Logo */}
-            <img src='https://www.koinx.com/_next/static/media/Logo.5f2ad8d5.svg' alt='KoinX Logo' />
+        <>
+            <div className='w-full flex justify-between items-center px-8 bg-white'>
+                {/* Logo */}
+                <img src='https://www.koinx.com/_next/static/media/Logo.5f2ad8d5.svg' alt='KoinX Logo' />
 
 
-            <div className='flex sm:gap-4 lg:gap-6 xl:gap-8 justify-center items-center'>
-                {/* Navigation */}
-                <nav className='hidden sm:block'>
-                    <ul className='flex sm:gap-2 lg:gap-4 xl:gap-6 font-semibold  text-base'>
+                <div className='flex sm:gap-4 lg:gap-6 xl:gap-8 justify-center items-center'>
+                    {/* Navigation */}
+                    <nav className='hidden sm:block'>
+                        <ul className='flex sm:gap-2 lg:gap-4 xl:gap-6 font-semibold  text-base'>
+                            <li className='cursor-pointer '>Crypto Taxes</li>
+                            <li className='cursor-pointer '>Free Tools</li>
+                            <li className='cursor-pointer '>Resource Center</li>
+                        </ul>
+                    </nav>
+
+                    {/* Button */}
+                    <button className='sm:block hidden text-white bg-[#0052fe] px-6 py-2 rounded-lg font-semibold  text-base'>Get Started</button>
+                    <button onClick={() => setIsOpen((open) => !open)}>
+                        {!isOpen ? <Hamburger className='sm:hidden' /> : <Close className='sm:hidden' />}
+                    </button>
+                </div>
+            </div>
+            {
+                isOpen &&
+                <div className={`h-32 w-full bg-white flex items-center justify-center flex-col border-t`}>
+                    <ul className='flex flex-col justify-center items-start font-semibold  text-base'>
                         <li className='cursor-pointer '>Crypto Taxes</li>
                         <li className='cursor-pointer '>Free Tools</li>
                         <li className='cursor-pointer '>Resource Center</li>
                     </ul>
-                </nav>
+                    <button className=' text-white bg-[#0052fe] px-6 py-2 rounded-lg font-semibold  text-base'>Get Started</button>
 
-                {/* Button */}
-                <button className='text-white bg-[#0052fe] px-6 py-2 rounded-lg font-semibold  text-base'>Get Started</button>
-            </div>
-        </div>
+                </div>
+            }
+
+        </>
     )
 }
 
--- src/utils/Icons.jsx
+++ src/utils/Icons.jsx
@@ -55,3 +55,18 @@ export function Hamburger({ className, onClick }) {
         </svg>
     )
 }
+
+export function Close({ className }) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" className={className}>
+            <g fill="none" stroke="currentColor" strokeDasharray="16" strokeDashoffset="16" strokeLinecap="round" strokeWidth="2">
+                <path d="M7 7L17 17">
+                    <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.4s" values="16;0" />
+                </path>
+                <path d="M17 7L7 17">
+                    <animate fill="freeze" attributeName="stroke-dashoffset" begin="0.4s" dur="0.4s" values="16;0" />
+                </path>
+            </g>
+        </svg>
+    )
+}
